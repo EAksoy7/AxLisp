dec7c2f87ecdb5bce2658b83c27f5986a43f5771
EAksoy7 <aksoyemin1@gmail.com>
2025-05-24 12:50:32 +0100

    Added command history

--- elisp.c
+++ elisp.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <editline/readline.h>
-#include <editline/history.h>
 
 /* Input buffer */
 int main(int argc, char* argv[]){
 
 	/* Version and exit info */
-	puts("Elisp version 0.0.0.0.1");
+	puts("Welcome to Elisp.");
 	puts("Press Ctrl+c to exit the interpreter");
 
 	while(1){
-
 		char* input = readline("Elisp> ");
 		add_history(input);
-		printf("You entered: %s", input);
+		printf("You entered: %s\n", input);
 		free(input);
 	}
 
