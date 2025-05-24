7ae35fe4c6850545dc6127132d905e2e6d01f7b5
EAksoy7 <aksoyemin1@gmail.com>
2025-05-24 14:42:46 +0100

    Added README and some very basic Windows compatability

--- elisp.c
+++ elisp.c
@@ -1,7 +1,28 @@
+#define VERSION "0.0.2"
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048]; 
+
+/* Placeholder readline function for Windows */
+char* readline(char* prompt){
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy) - 1] = '\0';
+	return cpy; 
+}
+
+/* Placeholder add_history function, make this do something */
+void add_history(char* unused){}
+
+#else
 #include <editline/readline.h>
-#define VERSION "0.0.2"
+#endif
 
 int main(int argc, char* argv[]){
 
