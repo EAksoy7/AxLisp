f3b50a3de16fa126b4a1967b5454df54e58fad8e
EAksoy7 <aksoyemin1@gmail.com>
2025-06-24 22:18:54 +0300

    Added very basic parsing, still work to do

--- elisp.c
+++ elisp.c
@@ -1,6 +1,7 @@
-#define VERSION "0.0.2"
+#define VERSION "0.0.3"
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -26,16 +27,46 @@ void add_history(char* unused){}
 
 int main(int argc, char* argv[]){
 
+	/* Parsers Declared */
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expression = mpc_new("expression");
+	mpc_parser_t* Elisp = mpc_new("elisp");
+
+	/* Parsers Defined */
+	mpca_lang(MPCA_LANG_DEFAULT, 
+	"																		\
+		number		:	/-?[0-9]+/ 				;							\
+		operator	:	'+' | '-' | '*' | '/' | '%'	;						\
+		expression	:	<number> | '(' <operator> <expression>+ ')' ;		\
+		elisp		:	/^/ <operator> <expression>+ /$/ ;					\
+	",
+	Number, Operator, Expression, Elisp);
+
 	/* Version and exit info */
 	printf("Welcome to Elisp, version %s\n", VERSION);
-	puts("Press Ctrl+c to exit the interpreter");
+	printf("Press Ctrl+c to exit the interpreter\n");
 
 	while(1){
 		char* input = readline("Elisp> ");
 		add_history(input);
-		printf("You entered: %s\n", input);
+
+		/* Begin Parsing */
+		mpc_result_t r;
+		if(mpc_parse("stdin", input, Elisp, &r)){
+			/* Success */
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			/* Error */
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+
 		free(input);
 	}
+
+	mpc_cleanup(4, Number, Operator, Expression, Elisp);
 
 	return 0;
 }
